22273061463792dfc90c88e951dfdd7cf1d35ac9
Hinga-vs <conradhinga@gmail.com>
2026-06-24 11:58:01 -0400

    MtaaniStore

---
+++ storeapp/urls.py
@@ -0,0 +1,21 @@
+
+from django.contrib import admin
+from django.urls import path
+from storeapp import views
+
+
+#url Mapping
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('home/', views.home),
+
+    path('about/', views.about),
+
+    path('contact/', views.contact),
+
+    path('gallery/', views.gallery),
+        
+        
+
+]
